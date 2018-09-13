90dfe7857157f2db83c9660ccf649403c85dd48d
18435616978 <842821853@qq.com>
2018-09-13 18:37:49 +0800

    18:37commit

--- src/components/A_news/new_brobot/a_mess/a_mess.vue
+++ src/components/A_news/new_brobot/a_mess/a_mess.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="new_detail">
         <div class="new_deta_main">
-            <span>企业公告由系统自动创建，所有企业成员都会自动加入到当前群组，该群组无法删除。</span>
+            <span class="new_tit">企业公告由系统自动创建，所有企业成员都会自动加入到当前群组，该群组无法删除。</span>
             <div>
                 <a><i class="iconfont icon-gongju"></i> 
                     添加集成服务
@@ -16,13 +16,106 @@
                    <span class="new_bind"> 9月10日 星期一</span>
                 </div>
            </div>
+           <div class="new_de_liuyan">
+               <ul>
+                    <li>
+                       <div class="new_message">
+                            <div class="new_ra_he">
+                                <span>M</span>
+                            </div>
+                            <div class="new_me_ri">
+                                <span>M</span>
+                                <em v-for='item of vList'>{{item.data}}</em>
+                                <i ref="i">...</i>
+                                <div class="new_me_r">
+                                    <span v-for="item of vList">{{item.con}}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                     <li v-for="(item,index) of N">
+                         <div class="new_message">
+                            <div class="new_ra_he">
+                                <span>M</span>
+                            </div>
+                            <div class="new_me_ri">
+                                <span>M</span>
+                                <em>{{item.data}}</em>
+                                <i @click='close1(index)'>{{item.i}}</i>
+                                <div class="new_me_r">
+                                    <span>{{item.con}}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+               </ul>
+           </div>
+        </div>
+        <div class="new_de_inp">
+            <span>
+                <i class="iconfont icon-xiaoliansmiley40"></i>
+                <i class="iconfont icon-paper-clip"></i>
+                <i class="iconfont icon-tianjia"></i>
+                <i class="iconfont icon-ai203"></i>
+            </span>
+            <input type="text" placeholder="按Enter键发送消息" ref="con" @keydown.enter="submit()">
         </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            vList:[
+                {
+                    con:'hello',
+                    data:'10:30',
+                    i:'...'
+                }
+            ],
+            N:[],
+            isShow:true,
+            // 之前时间
+            oldDate:'',
+            // 当前时间
+            newDate:''
+        }
+    },
+    methods:{
+        submit(){
+            var con = this.$refs.con.value;
+            var i = this.$refs.i.html;
+            var hours = new Date().getHours(); 
+            var minutes = new Date().getMinutes(); 
+            var seconds = new Date().getSeconds(); 
+            function zero(n){
+                return n.toString().length > 1 ? n.toString() : '0' + n.toString();
+            }
+            var str = hours+':'+zero(minutes)+':'+zero(seconds);
+            var getTime = new Date().getTime();
+			// console.log(date);
+            this.N.push({
+                con:con,
+                data:str,
+                i:'...'
+            })
+            localStorage.setItem('N',JSON.stringify(this.N));
+            this.isShow = !this.isShow;
+            this.$refs.con.value = '';
+        },
+        close1:function(index){
+            this.N.splice(index,1);
+            localStorage.setItem('N',JSON.stringify(this.L));
+            console.log(this.N)
+        },
+    },
+    created(){
+        var local = JSON.parse(localStorage.getItem('N'));
+        for(var i = 0;i < local.length;i++){
+            this.N.push(local[i]);
+        } 
+    }
 }
 </script>
 
@@ -36,6 +129,49 @@ export default {
     em,i{
         font-style: normal;
     }
+
+    .new_me_ri i{
+        margin-left: 10px;
+        color: #999;
+        cursor: pointer;
+        font-size: 18px;
+    }
+    .new_de_inp{
+        width: 81%;
+        height: 150px;
+        position: absolute;
+        z-index: 999;
+        border:1px solid #ccc;
+        background: #fff;
+        left: 14px;
+        bottom: 112px;
+    }
+    .new_de_inp span{
+        margin-left: 15px;  
+        margin-top:10px;
+        position: absolute;
+    }
+    .new_de_inp span i{
+        font-size: 18px;
+        color: #999;
+        margin-right: 15px;
+        cursor: pointer;
+    }
+    .new_de_inp input{
+        width: 96%;    
+        height: 100%;
+        outline: none;
+        border: none;
+        padding-left: 15px;
+    }
+    .new_tit{
+        color: #999;
+        font-size: 13px;
+        padding-top: 20px;
+        padding-left: 15px;
+        position: relative;
+        top: 9px;
+    }
     .new_detail{
         width: 100%;
         height:100%;
@@ -46,13 +182,14 @@ export default {
     }
     .new_deta_main{
         width: 81%;
-        height: 82%;
+        height: 64%;
         position: absolute;
         left: 1%;
         top: 3%;
         background: #fff;
+        overflow: scroll;
     }
-    .new_deta_main span{
+    .new_deta_main .new_bind{
         padding: 15px;
         color: #999;
         font-size: 14px;
@@ -104,5 +241,60 @@ export default {
         margin: 0;
         background-image: linear-gradient(to right,#fff,#eee 50%,#fff);
         background-repeat: no-repeat;
+    }
+    .new_message{
+        width: 70%;
+        height: 62px;
+        margin-left: 20px;
+        margin-top: 20px;
+        position: relative;
+    }
+    .new_ra_he{
+        width: 38px;
+        height: 38px;
+        border-radius: 50%;
+        background: #000;
+        background-color: rgb(239, 126, 222);
+        line-height: 38px;
+        text-align: center;
+        color: #fff;
+        font-size: 14px;
+        float: left;
+    }
+    .new_me_ri{
+        width: 80%;
+        position: absolute;
+        left: 50px;
+        font-size: 13px;
+    }
+
+    .new_me_ri span{
+        margin-right: 10px;
+    }
+    .new_me_ri em{
+        font-size: 12px;
+        color: #aaa;
+    }
+    .new_me_r{
+        width: 100%;
+        height: 40px;
+        background: transparent;
+        margin-top: 0px!important;
+        margin-left: -20px!important;
+    }
+    .new_me_r span{
+        line-height: 40px;
+        font-size: 15   px;
+        padding-left:20px;
+        color: #999;
+    }
+    .new_de_liuyan{
+        width: 95%;
+        height: 70%;
+        position: relative;
+        margin-top: 10px;
+    }
+    .new_de_liuyan li{
+        margin-top: -17px;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
-import "babel-polyfill"
-import 'vue2-animate/dist/vue2-animate.min.css';
+// import ElementUI from 'element-ui'
+// import '../node_modules/element-ui/lib/theme-chalk/index.css'
+
+// Vue.use(ElementUI)
+
 Vue.use(Vuex);
 
 Vue.use(VueRouter);
@@ -11,17 +14,10 @@ Vue.use(VueRouter);
 // 使用路由管理几个子组件
 //引入子组件
 const News = require('./components/A_news/news.vue');
-const Missions = require('./components/B_missions/missions.vue');
+const missions = require('./components/B_missions/missions.vue');
 const Calendar = require('./components/C_calendar/calendar.vue');
-const Driver = require('./components/D_driver/driver.vue');
-const Contact = require('./components/E_contact/contact.vue');
-
-
-
-// 日历子组件
-import all from './components/C_calendar/children/all.vue'
-import members from './components/C_calendar/children/members.vue'
-import my from './components/C_calendar/children/my.vue'
+const driver = require('./components/D_driver/driver.vue');
+const contact = require('./components/E_contact/contact.vue');
 
 //路由配置
 const r = [
@@ -142,364 +138,23 @@ const r = [
     },
     {
         path:'/missions',
-        component:Missions.default,
+        component:missions.default,
         name:'项目',
-        children:[
-            {
-                path: '/missions/subordinate',
-                name:'下属任务',
-                component: require('./components/B_missions/subordinate/subordinate.vue').default,
-            },
-            {
-                path: '/missions/my/',
-                name:'我的任务',
-                component: require('./components/B_missions/my/my.vue').default,
-                children:[
-                    {
-                        name: '我负责的我的任务',
-                        path: "/B_missions/my/directed",
-                        component: require('./components/B_missions/my/directed/directed.vue').default,
-                        children: [
-                            {
-                                name: '活动任务我负责的我的任务',
-                                path: "/B_missions/my/directed/active",
-                                component: require('./components/B_missions/my/directed/active/active.vue').default,
-                            },
-                            {
-                                name: '完成任务我负责的我的任务',
-                                path: "/B_missions/my/directed/finish",
-                                component: require('./components/B_missions/my/directed/finish/finish.vue').default,
-                            },
-                        ]
-                    },
-                    {
-                        name: '我分配的我的任务',
-                        path: "/B_missions/my/delegated",
-                        component: require('./components/B_missions/my/delegated/delegated.vue').default,
-                        children: [
-                            {
-                                name: '活动任务我分配的我的任务',
-                                path: "/B_missions/my/delegated/active",
-                                component: require('./components/B_missions/my/delegated/active/active.vue').default,
-                            },
-                            {
-                                name: '完成任务我分配的我的任务',
-                                path: "/B_missions/my/delegated/finish",
-                                component: require('./components/B_missions/my/delegated/finish/finish.vue').default,
-                            },
-                        ]
-                    },
-                    {
-                        name: '我参与的我的任务',
-                        path: "/B_missions/my/following",
-                        component: require('./components/B_missions/my/following/following.vue').default,
-                        children: [
-                            {
-                                name: '活动任务我参与的我的任务',
-                                path: "/B_missions/my/following/active",
-                                component: require('./components/B_missions/my/following/active/active.vue').default,
-                            },
-                            {
-                                name: '完成任务我参与的我的任务',
-                                path: "/B_missions/my/following/finish",
-                                component: require('./components/B_missions/my/following/finish/finish.vue').default,
-                            },
-                        ]
-                    },
-                    {
-                        name: '我创建的我的任务',
-                        path: "/B_missions/my/created",
-                        component: require('./components/B_missions/my/created/created.vue').default,
-                        children:[
-                            {
-                              name: '活动任务我创建的我的任务',
-                              path: "/B_missions/my/created/active",
-                              component: require('./components/B_missions/my/created/active/active.vue').default,
-                            },
-                            {
-                              name: '完成任务我创建的我的任务',
-                              path: "/B_missions/my/created/finish",
-                              component: require('./components/B_missions/my/created/finish/finish.vue').default,
-                            },
-                        ]
-                    }
-                ]
-            },
-            {
-                path: '/missions/configuration/',
-                name:'配置中心',
-                component: require('./components/B_missions/configuration/configuration.vue').default,
-                children:[
-                    {
-                        name:'项目配置中心',
-                        path:"/B_missions/configuration/project",
-                        component: require('./components/B_missions/configuration/project/project.vue').default,
-                        children: [
-                            {
-                                name: '项目模板项目配置中心',
-                                path: "/B_missions/configuration/project/templates",
-                                component: require('./components/B_missions/configuration/project/templates/templates.vue').default,
-                            },
-                            {
-                                name: '组建管理项目配置中心',
-                                path: "/B_missions/configuration/project/addon",
-                                component: require('./components/B_missions/configuration/project/addon/addon.vue').default,
-                            },
-                            {
-                                name: '报表管理项目配置中心',
-                                path: "/B_missions/configuration/project/insight",
-                                component: require('./components/B_missions/configuration/project/insight/insight.vue').default,
-                            },
-                            {
-                              path: '*',
-                              redirect: "/B_missions/configuration/project/templates"
-                            }
-                        ],
-                        
-                    },
-                    {
-                        name:'任务配置中心',
-                        path:"/B_missions/configuration/task",
-                        component: require('./components/B_missions/configuration/task/task.vue').default,
-                        children:[
-                            {
-                                name: '任务类型任务配置中心',
-                                path: "/B_missions/configuration/task/type-group",
-                                component: require('./components/B_missions/configuration/task/type-group/type-group.vue').default,
-                            },
-                            {
-                                name: '状态管理任务配置中心',
-                                path: "/B_missions/configuration/task/state",
-                                component: require('./components/B_missions/configuration/task/state/state.vue').default,
-                            },
-                            {
-                                name: '属性管理任务配置中心',
-                                path: "/B_missions/configuration/task/prop",
-                                component: require('./components/B_missions/configuration/task/prop/prop.vue').default,
-                            },
-                            {
-                                name: '数据管理任务配置中心',
-                                path: "/B_missions/configuration/task/datasource",
-                                component: require('./components/B_missions/configuration/task/datasource/datasource.vue').default,
-                            },
-                            {
-                                name: '标签管理任务配置中心',
-                                path: "/B_missions/configuration/task/tag-mode",
-                                component: require('./components/B_missions/configuration/task/tag-mode/tag-mode.vue').default,
-                            },
-                            {
-                              name: '优先级管理任务配置中心',
-                              path: "/B_missions/configuration/task/priority-mode",
-                              component: require('./components/B_missions/configuration/task/priority-mode/priority-mode.vue').default,
-                            },
-                            {
-                              path: '*',
-                              redirect: "/B_missions/configuration/task/type-group"
-                            }
-                        ]
-                    },
-                    {
-                        name:'安全配置中心',
-                        path:"/B_missions/configuration/security",
-                        component: require('./components/B_missions/configuration/security/security.vue').default,
-                        children:[
-                            {
-                                name: '角色管理安全配置中心',
-                                path: "/B_missions/configuration/security/role-mode",
-                                component: require('./components/B_missions/configuration/security/role-mode/role-mode.vue').default,
-                            },
-                            {
-                                name: '安全管理安全配置中心',
-                                path: "/B_missions/configuration/security/safe-mode",
-                                component: require('./components/B_missions/configuration/security/safe-mode/safe-mode.vue').default,
-                            },
-                            {
-                                name: '权限管理安全配置中心',
-                                path: "/B_missions/configuration/security/permission-mode",
-                                component: require('./components/B_missions/configuration/security/permission-mode/permission-mode.vue').default,
-                            },
-                            {
-                              path: '*',
-                              redirect: "/B_missions/configuration/security/role-mode"
-                            }
-                        ]
-                    },
-                    {
-                        name:'高级配置中心',
-                        path:"/B_missions/configuration/advance",
-                        component: require('./components/B_missions/configuration/advance/advance.vue').default,
-                        children:[
-                            {
-                                name: '提醒管理高级配置中心',
-                                path: "/B_missions/configuration/advance/reminder-mode",
-                                component: require('./components/B_missions/configuration/advance/reminder-mode/reminder-mode.vue').default,
-                            },
-                            {
-                                name: '通知管理高级配置中心',
-                                path: "/B_missions/configuration/advance/notification-mode",
-                                component: require('./components/B_missions/configuration/advance/notification-mode/notification-mode.vue').default,
-                            },
-                            {
-                                name: '事件高级配置中心',
-                                path: "/B_missions/configuration/advance/event",
-                                component: require('./components/B_missions/configuration/advance/event/event.vue').default,
-                            },
-                            {
-                              path: '*',
-                              redirect: "/B_missions/configuration/advance/reminder-mode"
-                            }
-                        ]
-                    },
-                    {
-                        name:'全局配置中心',
-                        path:"/B_missions/configuration/global",
-                        component: require('./components/B_missions/configuration/global/global.vue').default,
-                        children:[
-                            {
-                                name: '工作组建全局配置中心',
-                                path: "/B_missions/configuration/global/addons",
-                                component: require('./components/B_missions/configuration/global/addons/addons.vue').default,
-                            },
-                            {
-                              path: '*',
-                              redirect: "/B_missions/configuration/global/addons"
-                            }
-                        ]
-                    },
-                    {
-                        path:'*',
-                        redirect: "/B_missions/configuration/project/templates"
-                    }
-                ]
-            },
-            {
-                path: '/missions/trash',
-                name:'回收站',
-                component: require('./components/B_missions/trash/trash.vue').default,
-            },
-            
-            {
-              //如果用户随便输入地址,转到首页   给一个重定位
-              path: '*',
-              redirect: "/missions/configuration"
-            }
-        ]
     },
     {
         path:'/calendar',
         component:Calendar.default,
-        name:'日历',
-        children:[
-            {path:'/calendar/all',component:all},
-            {path:'/calendar/members',component:members},
-            {path:'/calendar/my',component:my},
-            {path:'/calendar',redirect: '/calendar/my'}
-        ]
+        name:'日历'
     },
     {
         path:'/driver',
-        component:Driver.default,
-        name:'网盘',
-        children:[
-			{
-				path:'/driver/qiye',
-				component:require('./components/D_driver/qiye/qiye.vue').default,
-				name:'企业管理',
-					children:[
-						// 配置子路由
-						{
-							path:'/driver/qiye/zhidu',
-							component:require('./components/D_driver/qiye/zhidu/zhidu.vue').default,
-							name:'公司制度'
-						},
-						{
-							path:'/driver/qiye/gongxiang',
-							component:require('./components/D_driver/qiye/gongxiang/gongxiang.vue').default,
-							name:'资料共享'
-						},
-						{
-							path:'/driver/qiye/guanli',
-							component:require('./components/D_driver/qiye/guanli/guanli.vue').default,
-							name:'缺陷管理'
-						}
-					]
-			},
-			{
-				path:'/driver/my',
-				component:require('./components/D_driver/my/my.vue').default,
-				name:'个人模板',
-					children:[
-						// 配置子路由
-						{
-							path:'/driver/my/person-child',
-							component:require('./components/D_driver/my/person-child/person-child.vue').default,
-							name:'person-child'
-						},
-					]
-				},
-				{
-					path:'/driver/share',
-					component:require('./components/D_driver/share/share.vue').default,
-					name:'分享管理'
-				},
-				{
-					path:'/driver/trash',
-					component:require('./components/D_driver/trash/trash.vue').default,
-					name:'回收站管理'
-				},
-				{
-					//如果用户随便输入地址,转到首页   给一个重定位
-						path:'/driver/',
-						redirect:'/driver/qiye'
-				}
-								
-			]
+        component:driver.default,
+        name:'网盘'
     },
     {
-        path:'/contact/',
-        component:Contact.default,
-        name:'通讯录',
-        children:[
-            {
-              path:'/contact/rc',
-              component:require('./components/E_contact/rc/rc.vue').default,
-              name:"日程助手",
-            },
-            {
-                path:'/contact/wp',
-                component:require('./components/E_contact/wp/wp.vue').default,
-                name:"网盘助手",
-            },
-            {
-                path:'/contact/xm',
-                component:require('./components/E_contact/xm/xm.vue').default,
-                name:"项目助手",
-            },
-            {
-                path:'/contact/xt',
-                component:require('./components/E_contact/xt/xt.vue').default,
-                name:"小特机器人",
-            },
-            {
-                path:'/contact/M',
-                component:require('./components/E_contact/M/m.vue').default,
-                name:"m",
-            },
-            {
-                path:'/contact/channels/123',
-                component:require('./components/E_contact/channels/123.vue').default,
-                name:"123",
-            },
-            {
-                path:'/contact/channels/forth',
-                component:require('./components/E_contact/channels/forth.vue').default,
-                name:"四组",
-            },
-            {
-                path:'/contact/',
-                redirect:'/contact/M'
-            }
-          ]
+        path:'/contact',
+        component:contact.default,
+        name:'通讯录'
     },
     {
       //如果用户随便输入地址,转到首页   给一个重定位
@@ -511,41 +166,13 @@ const r = [
 //路由初始化
 const router = new VueRouter({
     // 这里的routes 建必须这么写
-   routes : r,
-   mode:'history'
+   routes : r
 });
 
 //配置vuex
 const store = new Vuex.Store({
     state:{
-      count:1,
-      member:[]
-    },
-    mutations:{
-        ADD(state,payload){
-            state.member.push(payload);
-         },
-         GETALL(state,payload){
-            state.member=payload;
-        },
-    },
-    actions:{
-        async GETALL(context,payload){
-            //请求数据
-            var data=await fetch('../mapList/').then(res=>res.json());
-            context.commit('GETALL',data);
-        },
-        async ADD({commit},payload){
-            //上传数据
-            var data=await fetch('../mapList/',{
-                "method":"POST",
-                "headers":{
-                    "Content-Type":"application/json"
-                },
-                "body":JSON.stringify(payload)
-            }).then(res=>res.json());
-            commit('ADD',data);
-        },
+      count:1
     }
 });
 
